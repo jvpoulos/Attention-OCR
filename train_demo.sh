7282def573cf91f5623452569ea689f5e1ee280a
jvpoulos <jvpoulos@gmail.com>
2017-03-15 10:45:37 -0700

    update to Tensorflow 1 with Sivanke pull requests: https://github.com/EliotAndres/Attention-OCR

--- train_demo.sh
+++ train_demo.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 python src/launcher.py \
 	--phase=train \
-	--data-path=/home/jeremy/data/Synthetic_Word_Dataset/90kDICT32px/new_annotation_train.txt \
-	--data-base-dir=/home/jeremy/data/Synthetic_Word_Dataset/90kDICT32px \
-	--log-path=log_sy.txt \
+	--data-path=/media/data2/sivankeret/Datasets/mnt/ramdisk/max/90kDICT32px/annotation_train_words.txt \
+	--data-base-dir=/media/data2/sivankeret/Datasets/mnt/ramdisk/max/90kDICT32px \
+	--log-path=log_01_16.txt \
 	--attn-num-hidden 256 \
 	--batch-size 64 \
-	--model-dir=model_x \
+	--model-dir=model_01_16 \
 	--initial-learning-rate=1.0 \
-	--num-epoch=20000 \
+	--no-load-model \
+	--num-epoch=3 \
 	--gpu-id=0 \
 	--use-gru \
-        --target-embedding-size=10
-
+	--steps-per-checkpoint=2000 \
+    --target-embedding-size=10
