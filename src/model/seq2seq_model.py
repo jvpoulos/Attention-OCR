d8e87e32d211231c5ff7b8297403629354e9e2ed
jvpoulos <jvpoulos@gmail.com>
2017-04-04 09:27:49 -0700

    add dropout to multilayer decoder

--- src/model/seq2seq_model.py
+++ src/model/seq2seq_model.py
@@ -94,7 +94,8 @@ class Seq2SeqModel(object):
         cell = single_cell
 
         if attn_num_layers > 1:
-            cell = tf.contrib.rnn.MultiRNNCell([single_cell] * attn_num_layers, state_is_tuple=False)
+            cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=0.5) # add dropout for multilayer decoder
+            cell = tf.contrib.rnn.MultiRNNCell([cell] * attn_num_layers, state_is_tuple=False)
 
         # The seq2seq function: we use embedding for the input and attention.
         def seq2seq_f(lstm_inputs, decoder_inputs, seq_length, do_decode):
