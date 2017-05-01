088f104a180ed9690600e94ed481c3553f9d01b6
jvpoulos <jvpoulos@gmail.com>
2017-05-01 15:20:53 -0700

    play w buckets

--- src/data_util/data_gen.py
+++ src/data_util/data_gen.py
@@ -36,11 +36,11 @@ class DataGen(object):
         if evaluate:
             self.bucket_specs = [(int(math.floor(64 / 4)), int(word_len + 2)), (int(math.floor(108 / 4)), int(word_len + 2)),
                                  (int(math.floor(140 / 4)), int(word_len + 2)), (int(math.floor(256 / 4)), int(word_len + 2)),
-                                 (int(math.floor(img_width_range[1] / 4)), 32)]
+                                 (int(math.floor(img_width_range[1] / 4)), int(word_len + 2))]
         else:
             self.bucket_specs = [(int(64 / 4), 9 + 2), (int(108 / 4), 15 + 2),
                              (int(140 / 4), 17 + 2), (int(256 / 4), 20 + 2),
-                             (int(math.ceil(img_width_range[1] / 4)), 32)]
+                             (int(math.ceil(img_width_range[1] / 4)), int(word_len + 2))]
 
         self.bucket_min_width, self.bucket_max_width = img_width_range
         self.image_height = img_height
