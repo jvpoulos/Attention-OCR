47763d6383712e2b5247bc236bbd1b957916f325
jvpoulos <jvpoulos@gmail.com>
2017-07-19 09:59:52 -0400

    fix buckets, inc. cnn layers to 10 layers

--- src/data_util/data_gen.py
+++ src/data_util/data_gen.py
@@ -32,13 +32,13 @@ class DataGen(object):
             self.annotation_path = os.path.join(data_root, annotation_fn)
 
         if evaluate:
-            self.bucket_specs = [(int(math.floor(img_width_range[0])), int(math.ceil(img_width_range[1] / 5))),
+            self.bucket_specs = [(int(math.floor(img_width_range[0])), int(math.ceil(img_width_range[1] / 7))),
                              # (int(math.ceil(img_width_range[1] / 9)), int(math.ceil(img_width_range[1] / 8))),
                              # (int(math.ceil(img_width_range[1] / 7)), int(math.ceil(img_width_range[1] / 6))), 
                              # (int(math.ceil(img_width_range[1] / 5)), int(math.ceil(img_width_range[1] / 4))),
                              (int(math.ceil(img_width_range[1] / 6)), int(math.ceil(img_width_range[1] / 4)))]
         else:
-            self.bucket_specs = [(int(math.floor(img_width_range[0])), int(math.ceil(img_width_range[1] / 5))),
+            self.bucket_specs = [(int(math.floor(img_width_range[0])), int(math.ceil(img_width_range[1] / 7))),
                              # (int(math.ceil(img_width_range[1] / 9)), int(math.ceil(img_width_range[1] / 8))),
                              # (int(math.ceil(img_width_range[1] / 7)), int(math.ceil(img_width_range[1] / 6))), 
                              # (int(math.ceil(img_width_range[1] / 5)), int(math.ceil(img_width_range[1] / 4))),
